054eb28cfd2acaf0d688ab64ff418203e07e2378
Daniel Nägele <daniel@naegele.dev>
2025-07-25 15:17:23 +0200

    dart run build_runner build

---
+++ lib/test.tailor.dart
@@ -0,0 +1,46 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'test.dart';
+
+// **************************************************************************
+// TailorAnnotationsGenerator
+// **************************************************************************
+
+mixin _$TestThemeTailorMixin on ThemeExtension<TestTheme> {
+  InvalidType get someColor;
+
+  @override
+  TestTheme copyWith({InvalidType? someColor}) {
+    return TestTheme(someColor: someColor ?? this.someColor);
+  }
+
+  @override
+  TestTheme lerp(covariant ThemeExtension<TestTheme>? other, double t) {
+    if (other is! TestTheme) return this as TestTheme;
+    return TestTheme(someColor: t < 0.5 ? someColor : other.someColor);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is TestTheme &&
+            const DeepCollectionEquality().equals(someColor, other.someColor));
+  }
+
+  @override
+  int get hashCode {
+    return Object.hash(
+      runtimeType.hashCode,
+      const DeepCollectionEquality().hash(someColor),
+    );
+  }
+}
+
+extension TestThemeBuildContextProps on BuildContext {
+  TestTheme get testTheme => Theme.of(this).extension<TestTheme>()!;
+  InvalidType get someColor => testTheme.someColor;
+}
