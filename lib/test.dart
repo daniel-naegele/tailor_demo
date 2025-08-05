726f69381f840e532a8216c7dcc68382505d7fa6
Daniel Nägele <daniel@naegele.dev>
2025-08-05 16:36:15 +0200

    fix testtheme

--- lib/test.dart
+++ lib/test.dart
@@ -1,12 +1,11 @@
 import 'package:theme_tailor_annotation/theme_tailor_annotation.dart';
+import 'package:flutter/material.dart';
 
 part 'test.tailor.dart';
 
 @tailorMixin
-class TestTheme extends ThemeExtension<TestTheme> with _$TestTheme {
-  const TestTheme({
-    this.someColor,
-  });
+class TestTheme extends ThemeExtension<TestTheme> with _$TestThemeTailorMixin {
+  const TestTheme({this.someColor});
 
   final Color? someColor;
 }
